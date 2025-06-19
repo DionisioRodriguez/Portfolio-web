195d0c355d99801b14bb5e9dd54dbe9ecf2bc4f6
DionisioRodriguez <dionisio.joaquin.rodriguez@gmail.com>
2025-06-18 21:27:00 -0300

    Refactor AboutMe, Contact, Presentation, and Proyectos components; update styles, structure, and add new icons

---
+++ src/consts/contacto.js
@@ -0,0 +1,24 @@
+import Github from "@icons/Github.astro";
+import LinkedIn from "@icons/LinkedIn.astro";
+import Mail from "@icons/Mail2.astro";
+
+export const APLICACIONES = [
+  {
+    name: "LinkedIn",
+    info: "/in/DionisioRodriguez",
+    icon: LinkedIn,
+    href: ""
+  },
+  {
+    name: "GitHub",
+    info: "/DionisioRodriguez",
+    icon: Github,
+    href: "https://github.com/DionisioRodriguez?tab=repositories"
+  },
+  {
+    name: "Mail",
+    info: "/dionisio.joaquin.rodriguez@gamil.com",
+    icon: Mail,
+    href: "mailto:dionisio.joaquin.rodriguez@gamil.com"
+  } 
+]
